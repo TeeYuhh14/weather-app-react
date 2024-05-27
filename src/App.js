ea1ac6234aa75f131ef9e845a4080857af409032
TeeYuhh14 <tiademas@gmail.com>
2024-05-27 22:26:10 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tia Demas and is{" "}
-        <a
-          href="https://github.com/TeeYuhh14/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Tia Demas and is{" "}
+          <a
+            href="https://github.com/TeeYuhh14/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
