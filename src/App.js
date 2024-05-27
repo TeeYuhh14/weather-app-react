e4c90856e929ae30cb12420b2e8f02b95aa980b8
TeeYuhh14 <tiademas@gmail.com>
2024-05-27 21:35:54 +0200

    Added GitHub repository URL to footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tia Demas and is{" "}
+        <a
+          href="https://github.com/TeeYuhh14/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
